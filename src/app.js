0c0708c674f43b1a469b19787bfdbb65983e799c
CarlosPuga <c.puga26@gmail.com>
2024-04-24 10:48:37 -0400

    ExcuseGenerator

--- src/app.js
+++ src/app.js
@@ -6,6 +6,33 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let excuse = document.getElementById("excuse");
+  let frase = "";
+  let who = ["The dog ", "My grandma ", "The mailman ", "My bird "];
+  let action = ["ate ", "peed ", "crushed ", "broke "];
+  let what = ["my homework ", "my phone ", "the car "];
+  let when = [
+    "before the class",
+    "when I was sleeping",
+    "while I was exercising",
+    "during my lunch",
+    "while I was praying"
+  ];
+
+  let whoIndex = Math.floor(Math.random() * who.length);
+  let actionIndex = Math.floor(Math.random() * action.length);
+  let whatIndex = Math.floor(Math.random() * what.length);
+  let whenIndex = Math.floor(Math.random() * when.length);
+
+  let whotext = who[whoIndex];
+  let actiontext = action[actionIndex];
+  let whattext = what[whatIndex];
+  let whentext = when[whenIndex];
+
+  frase += whotext + actiontext + whattext + whentext + " ";
+
+  excuse.textContent = frase;
+  excuse.style.fontSize = "50px";
+  excuse.style.textAlign = "center";
+  console.log(frase);
 };
